167159e35521443b819d933d1df4c80f7fddb83c
unknown <pb132001@gmail.com>
2024-03-21 12:50:59 +0530

    intial commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import'./bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import todoStore from './Redux/Slices/todoStore.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <BrowserRouter><Provider store={todoStore}> <App /></Provider></BrowserRouter>
   </React.StrictMode>,
 )
